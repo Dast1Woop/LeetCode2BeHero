905e3db9980629bdfd147af8049ef09a7bdd91cb
myl <myldast1@126.com>
2021-07-22 11:36:08 +0800

    新增双指针法。综合性对比：两层for循环法（intersect） < 借助字典法（intersect1） < 双指针法（intersect2）

--- 两个数组的交集 II/两个数组的交集 II/main.swift
+++ 两个数组的交集 II/两个数组的交集 II/main.swift
@@ -35,21 +35,38 @@
 
 import Foundation
 
+/*综合性对比：两层for循环法（intersect） < 借助字典法（intersect1） < 双指针法（intersect2）
+ */
 func intersect(_ nums1: [Int], _ nums2: [Int]) -> [Int] {
     let nums1 = nums1.sorted()
     let nums2 = nums2.sorted()
     if nums1.count >= nums2.count {
 //       return subInterseect(longArr: nums1, shortArr: nums2)
+//        return subInterseect1(longArr: nums1, shortArr: nums2)
         return subInterseect2(longArr: nums1, shortArr: nums2)
     }else{
 //       return subInterseect(longArr: nums2, shortArr: nums1)
+//        return subInterseect1(longArr: nums2, shortArr: nums1)
         return subInterseect2(longArr: nums2, shortArr: nums1)
     }
 }
 
+/*执行用时：
+ 36 ms
+ , 在所有 Swift 提交中击败了
+ 49.38%
+ 的用户
+ 内存消耗：
+ 13.9 MB
+ , 在所有 Swift 提交中击败了
+ 21.88%
+ 的用户
+ */
 func subInterseect(longArr:[Int], shortArr:[Int])-> [Int]{
     var arr = [Int]()
     var index = 0
+    
+    //跟双指针相比，不够高效，可能多很多次没必要的对比。eg：[1,5,6,10],[1,7,10],7-》10时，10跟5、6的对比就是没必要的。
     for i in 0..<shortArr.count {
         for j in index..<longArr.count{
             if shortArr[i] == longArr[j] {
@@ -64,7 +81,19 @@ func subInterseect(longArr:[Int], shortArr:[Int])-> [Int]{
     return arr
 }
 
-func subInterseect2(longArr:[Int], shortArr:[Int]) -> [Int]{
+
+/*执行用时：
+ 32 ms
+ , 在所有 Swift 提交中击败了
+ 54.38%
+ 的用户
+ 内存消耗：
+ 14.1 MB
+ , 在所有 Swift 提交中击败了
+ 5.00%
+ 的用户
+ */
+func subInterseect1(longArr:[Int], shortArr:[Int]) -> [Int]{
     var arr = [Int]()
     var dic = [Int:Int]()
     for i in 0..<longArr.count{
@@ -89,10 +118,43 @@ func subInterseect2(longArr:[Int], shortArr:[Int]) -> [Int]{
     return arr
 }
 
-let arr0 = intersect([1,2,2,1], [2,2])
+/*双指针法
+ 执行用时：
+ 28 ms
+ , 在所有 Swift 提交中击败了
+ 75.63%
+ 的用户
+ 内存消耗：
+ 13.8 MB
+ , 在所有 Swift 提交中击败了
+ 50.00%
+ 的用户
+ */
+func subInterseect2(longArr:[Int], shortArr:[Int])-> [Int]{
+    var iShort = 0, jLong = 0
+    var arr = [Int]()
+    while iShort < shortArr.count && jLong < longArr.count {
+        let itemS = shortArr[iShort]
+        let itemL = longArr[jLong]
+        
+        if itemS == itemL {
+            arr.insert(itemS, at: 0)
+            iShort += 1
+            jLong += 1
+        }else if itemS < itemL{
+            iShort += 1
+        }else{//itemS > itemL
+            jLong += 1
+        }
+    }
+    
+    return arr
+}
+
+let arr0 = intersect([2,2], [1,2,2,1])
 print(arr0)
 
-let arr1 = intersect([1,2,2,1], [2,2,2])
+let arr1 = intersect([2,2,2], [1,2,2,1])
 print(arr1)
 
 let arr02 = intersect([4,9,5], [9,4,9,8,4])
